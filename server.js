bb222ef856f42fea023f2af2b694fb86747d365c
PROMENADE\eper.norbert <eper.norbert@promenade.hu>
2021-10-07 13:42:30 +0200

    code cleaning

--- server.js
+++ server.js
@@ -19,20 +19,17 @@ server.listen(5000);
 io = socketIO(server);
 
 io.on('connection', function (socket) {
-  socket.emit('greeting-from-server', {
-      greeting: 'Hello Client'
-  });
-  socket.on('greeting-from-client', function (message) {
-    console.log(message);
-  });  
-});
-
-io.on('connection', (socket) => {
-    console.log('success-connection')
+    socket.emit('greeting-from-server', {
+        greeting: 'Hello Client'
+    });
+    socket.on('greeting-from-client', function (message) {
+        console.log(message);
+    }); 
+    //console.log('success-connection')
     socket.on('chat message', msg => {
-      io.emit('chat message', msg);
+        io.emit('chat message', msg);
     });  
-  });
+});
 
 // handle timesync requests
 app.use('/timesync', timesyncServer.requestHandler);
