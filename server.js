bd54cb1d118271cda0ec0ca416e6991681f73b1a
PROMENADE\eper.norbert <eper.norbert@promenade.hu>
2021-10-07 10:04:38 +0200

    socket io and express server work fine, include script.js also work fine :))

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIO = require('socket.io'),
+    server, io;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server = http.Server(app);
+server.listen(5000);
+
+io = socketIO(server);
+
+io.on('connection', function (socket) {
+  socket.emit('greeting-from-server', {
+      greeting: 'Hello Client'
+  });
+  socket.on('greeting-from-client', function (message) {
+    console.log(message);
+  });
+});
+
+app.use('/public', express.static(__dirname + '/public'));
