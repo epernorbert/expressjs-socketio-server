542d119a7dbc5a69fd9a7730076fb53fa34bfd0b
PROMENADE\eper.norbert <eper.norbert@promenade.hu>
2021-10-08 16:15:34 +0200

    database connection

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+/* database connection begin */
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'countdown'
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM countdown', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log(rows)
+})
+
+
 var timesyncServer = require('timesync/server');
 var express = require('express'),
     app = express(),
@@ -21,7 +39,7 @@ io = socketIO(server);
 io.on('connection', function (socket) {    
     console.log('success-connection')
     socket.on('send-minute', ({status, inputValue, clickSend}) => {
-        io.emit('send-minute', ({status, inputValue, clickSend}));
+        io.emit('send-minute', ({status, inputValue, clickSend}));                
     }); 
     socket.on('send-start', ({status, clickStart, endTime}) => {
         io.emit('send-start', ({status, clickStart, endTime}));
@@ -32,3 +50,6 @@ io.on('connection', function (socket) {
 app.use('/timesync', timesyncServer.requestHandler);
 
 app.use('/public', express.static(__dirname + '/public'));
+
+connection.end()
+/* database connection end */
